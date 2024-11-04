c75350341bb1316498487e3154cd4c01e517741b
wingSkyG <1392004426@qq.com>
2024-11-04 19:00:06 +0800

    no message

---
+++ Assets/Scripts/Script/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject gameObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var name = gameObject.transform.GetChild(0).name;
+        Debug.Log(name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
